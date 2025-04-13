5d066ce13de050404d42d4bfe400cfa20cc1bbd1
mynamesdave <davislouie1@gmail.com>
2025-04-12 20:39:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+
+st.title("🎈 My new app")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+
+import streamlit as st
 import yfinance as yf
 import numpy as np
 import math
